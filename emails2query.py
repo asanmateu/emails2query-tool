ed8754696ace97e026279991f23e83cb1393aaff
toni <antonio.sanmateu@gmail.com>
2021-01-16 21:12:04 +0100

    add: error messages

--- emails2query.py
+++ emails2query.py
@@ -15,9 +15,10 @@ try:
     df = pd.read_excel("emails2query_template.xlsx")
 
 except IndexError:
-    print("Please, make sure you use and .xlsx file that it is saved as Excel Workbook and NOT as "
-          "Strict Open XML Spreadsheet and try again. \n")
+    print("Error: make sure you use and .xlsx file that it is saved as Excel Workbook and NOT as "
+          "Strict Open XML Spreadsheet and try again.")
     sys.exit()
+
 
 # Format null values to be easily manipulated
 df.replace('nan', np.nan, inplace=True)
@@ -29,31 +30,40 @@ regex = r"[a-zA-Z0-9_.+-]*@[a-zA-Z0-9-]+.[a-zA-Z\.]*"
 # Initialise empty list to add query ready emails
 emails_query_format = []
 
-# Iterate over retailer_id / emails template rows and append formatted emails to list
-for i, row in df.iterrows():
-    # Put all emails in the row into a list
-    emails = re.findall(regex, df['additional_emails'][i])
-    emails = [email.strip() for email in emails]
+try:
+    print("Transforming additional buyer names and emails into query format...")
 
-    # Put all additional buyers into a list
-    buyer_names = row['additional_buyers']
-    buyers = re.split(r";", buyer_names)
-    buyers = [buyer.strip() for buyer in buyers]
+    # Iterate over retailer_id / emails template rows and append formatted emails to list
+    for i, row in df.iterrows():
+        # Put all emails in the row into a list
+        emails = re.findall(regex, df['additional_emails'][i])
+        emails = [email.strip() for email in emails]
 
-    # Empty list for buyer-email tuples
-    buyer_emails_tuple_list = []
+        # Put all additional buyers into a list
+        buyer_names = row['additional_buyers']
+        buyers = re.split(r";", buyer_names)
+        buyers = [buyer.strip() for buyer in buyers]
 
-    # Pair emails and names even when buyer names are missing then use empty string...
-    for idx in range(len(emails)):
-        if idx <= len(buyers) - 1:
-            buyer_emails_tuple_list.append((buyers[idx], emails[idx]))
-        elif idx > len(buyers) - 1:
-            buyer_emails_tuple_list.append(("", emails[idx]))
+        # Empty list for buyer-email tuples
+        buyer_emails_tuple_list = []
 
-    # Sub the value pairs on each tuple in buyer-emails tuple list into query format and append to overall list...
-    for tup in buyer_emails_tuple_list:
-        query_format = "(" + str(row['retailer_id']) + "," + "'" + tup[0] + "'" + "," + "'" + tup[1] + "'" + ")" + ","
-        emails_query_format.append(query_format)
+        # Pair emails and names even when buyer names are missing then use empty string...
+        for idx in range(len(emails)):
+            if idx <= len(buyers) - 1:
+                buyer_emails_tuple_list.append((buyers[idx], emails[idx]))
+            elif idx > len(buyers) - 1:
+                buyer_emails_tuple_list.append(("", emails[idx]))
+
+        # Sub the value pairs on each tuple in buyer-emails tuple list into query format and append to overall list...
+        for tup in buyer_emails_tuple_list:
+            query_format = "(" + str(row['retailer_id']) + "," + "'" + tup[0] + "'" + "," + "'" + tup[1] + "'" + ")" + ","
+            emails_query_format.append(query_format)
+
+except KeyError:
+    print("Error: please check that you template contains the right columns (retailer_id, additional_buyers, "
+          "additional_emails) and try again.")
+    sys.exit()
+
 
 # New DataFrame to input query ready emails
 query_df = pd.DataFrame(emails_query_format, columns=['emails'])
