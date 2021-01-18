4cbdac5181d923b268e00f435ac31073e1be87a2
toni <antonio.sanmateu@gmail.com>
2021-01-18 09:54:55 +0100

    add: error message constant

---
+++ constants/messages.py
@@ -0,0 +1,5 @@
+IMPORT_INDEX_ERROR = "Error: make sure you use and .xlsx file that it is saved as Excel Workbook and NOT as " \
+              "Strict Open XML Spreadsheet and try again."
+
+COLUMN_KEY_ERROR = "Error: please check that you template contains the right columns (retailer_id, " \
+                   "additional_buyers, additional_emails) and try again."
