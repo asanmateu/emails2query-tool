2c20ceadaa2577a91a567c19a59ca27d3827ce38
toni <antonio.sanmateu@gmail.com>
2021-01-18 09:55:05 +0100

    add: necessary paths

---
+++ constants/paths.py
@@ -0,0 +1,5 @@
+from constants import os
+
+CURRENT_PATH = os.getcwd()
+FILE_PATH = CURRENT_PATH + "/input/emails2query_template.xlsx"
+OUTPUT_PATH = CURRENT_PATH + "/output/emails2query_ready.xlsx"
