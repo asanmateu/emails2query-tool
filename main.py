7a4420a88dcf4d12b6a2ffec582d921a0ee44e14
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2021-03-23 16:01:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from utils import *
 
 
 def main():
-    df = read_excel_template(INPUT_FILE_NAME)
-    format_null_values(df)
-    emails_query_format = get_all_pairs(df)
-    export_results(emails_query_format, OUTPUT_PATH, OUTPUT_FILE_NAME)
+    df = format_null_values(read_excel_template(INPUT_PATH, INPUT_FILE_NAME))
+    export_results(generate_all_pairs(df), OUTPUT_PATH, OUTPUT_FILE_NAME)
 
 
 if __name__ == "__main__":
